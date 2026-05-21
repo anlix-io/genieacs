c60b53b2ce5dc1493bd789dea140c005350ef150
lucasperovani <lucasperovani@gmail.com>
2026-05-21 16:28:15 -0300

    Fixed missing URI encode

--- ext/flashman-api.js
+++ ext/flashman-api.js
@@ -710,7 +710,8 @@ const getFirmwareFile = async function(args, callback) {
   // Call Flashman
   let response = await sendFlashmanRequest(
     'GET',
-    `product-class/${params.productClass}/version/${params.version}/firmware`,
+    `product-class/${encodeURIComponent(params.productClass)}/version/` +
+      `${encodeURIComponent(params.version)}/firmware`,
     {},
   );
 
